3c790f19578744f0778640ee82bc6733a3b6fc38
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-08 14:50:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
   elsif array.size >= 3
-    array.pop.join(", ")
+    array.join(", ")
     array[-1].join(" and ")
   end
 end
